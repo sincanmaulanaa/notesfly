6266c9e164d6994963c524e2720b3f9b3fecea0f
Sincan Maulana <sincanmaulanaa@gmail.com>
2025-03-29 01:23:37 +0700

    feat: add favicons support with favicons-webpack-plugin and update index.html metadata

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const favIconsWebpackPlugin = require('favicons-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 module.exports = {
   entry: './src/app.js',
@@ -27,5 +29,6 @@ module.exports = {
       template: './index.html',
       filename: 'index.html',
     }),
+    new FaviconsWebpackPlugin('./src/assets/app-icon.png'),
   ],
 };
